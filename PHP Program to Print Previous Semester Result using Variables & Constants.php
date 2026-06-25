be2253678346e88d87da7c06c3f15c764f635aca
alan083 <alanthomas.132652@marwadiuniversity.ac.in>
2026-06-25 12:15:25 +0530

    Add files via upload

---
+++ PHP Program to Print Previous Semester Result using Variables & Constants.php
@@ -0,0 +1,12 @@
+<?php
+define("COLLEGE_NAME", "Marwadi University");
+
+$studentName = "Mikey";
+$semester = 1;
+$percentage = 78.5;
+
+echo "College Name: " . COLLEGE_NAME . "<br>";
+echo "Student Name: " . $studentName . "<br>";
+echo "Semester: " . $semester . "<br>";
+echo "Percentage: " . $percentage . "%";
+?>
