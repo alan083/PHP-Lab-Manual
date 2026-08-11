e2b790fa32e95aa3483b1778174685393262257e
alan083 <alanthomas.132652@marwadiuniversity.ac.in>
2026-08-11 09:30:29 +0530

    Add files via upload

---
+++ cookie_example.php
@@ -0,0 +1,25 @@
+<?php
+
+// function encrypt_cookie($user_value)
+// {
+//      $user_value = $user_value .  "434873847";
+//      $data = setcookie("user", "$user_value", time() + 300);
+//      return $data;
+// }
+
+// cookie is a persistant storage but with a expiry timeline.
+// for performance tracing of website
+// store Information related to user search data
+// data stored in cookies converted to tables.
+
+// creating cookie
+// setcookie("user", "Ashish", time() + 300);
+
+// editing cookie
+// setcookie("user", "developer", time() + 300);
+
+//deleting cookie
+
+setcookie("user","Ashish",time()+300);
+
+?>
