3b46c3044ec235d448e22298a18cb7fb72c6fb44
alan083 <alanthomas.132652@marwadiuniversity.ac.in>
2026-08-11 09:35:10 +0530

    Add files via upload

---
+++ input.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>User Input</title>
+</head>
+<body>
+
+<form method="post">
+    Enter Your Name:
+    <input type="text" name="username">
+    <input type="submit" value="Submit">
+</form>
+
+<?php
+if(isset($_POST['username']))
+{
+    $name = $_POST['username'];
+    echo "<h3>Welcome, $name</h3>";
+}
+?>
+
+</body>
+</html>
