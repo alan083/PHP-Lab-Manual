3e45f4542ead50b67137e14846e4fe9c834920c2
alan083 <alanthomas.132652@marwadiuniversity.ac.in>
2026-08-11 09:36:28 +0530

    Add files via upload

---
+++ calpercentfunction.php
@@ -0,0 +1,15 @@
+<?php
+function calculatePercentage($m1, $m2, $m3, $m4, $m5)
+{
+    $total = $m1 + $m2 + $m3 + $m4 + $m5;
+    $percentage = ($total / 500) * 100;
+
+    echo "Total Marks: $total<br>";
+
+    return $percentage;
+}
+
+$percentage = calculatePercentage(85, 78, 90, 88, 80);
+
+echo "Percentage: $percentage%";
+?>
