d4ec3109ed30ffc2296f06b218f0e81bbcc543a8
alan083 <alanthomas.132652@marwadiuniversity.ac.in>
2026-08-11 09:38:38 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    This is rendered using apache and providing it to the browser.
+    <br>
+    <?php echo "this is php code rendered using apache and providing into browser."; ?> 
+</body>
+</html>
