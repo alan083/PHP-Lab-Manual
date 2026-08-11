7130f13d3e8c1aa10198bae3873180a4d72cd14b
alan083 <alanthomas.132652@marwadiuniversity.ac.in>
2026-08-11 09:38:01 +0530

    Add files via upload

---
+++ minmax.php
@@ -0,0 +1,17 @@
+<?php
+// Declare numbers
+$num1 = 25;
+$num2 = 50;
+$num3 = 15;
+
+// Find maximum number
+$max = max($num1, $num2, $num3);
+
+// Find minimum number
+$min = min($num1, $num2, $num3);
+
+// Display results
+echo "Numbers are: $num1, $num2, $num3 <br>";
+echo "Maximum Number = " . $max . "<br>";
+echo "Minimum Number = " . $min;
+?>
