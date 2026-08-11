33f5abb508ac0c13184ec759acbb896e4ce701ed
alan083 <alanthomas.132652@marwadiuniversity.ac.in>
2026-08-11 09:36:06 +0530

    Add files via upload

---
+++ functionexists.php
@@ -0,0 +1,15 @@
+<?php
+function welcomeStudent()
+{
+    echo "Welcome Student!";
+}
+
+if(function_exists("welcomeStudent"))
+{
+    welcomeStudent();
+}
+else
+{
+    echo "Error: Function does not exist.";
+}
+?>
